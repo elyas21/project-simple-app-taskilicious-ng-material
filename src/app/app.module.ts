9d5c6175786d4d6701233bc25fcb0f142308a2b4
unknown <elyasabate21@gmail.com>
2023-02-17 17:37:27 +0300

    (Extra) Category Sort Dropdown: 2SP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 
 import { AppComponent } from './app.component';
@@ -39,8 +40,8 @@ import { CategoryDetailComponent } from './compoenets/category-detail/category-d
     MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatFormFieldModule
-    
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/compoenets/category-list/category-list.component.ts
+++ src/app/compoenets/category-list/category-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { BehaviorSubject, map, Observable, Subject, switchMap, tap } from 'rxjs';
 import { Category } from 'src/app/models/Category';
 import { CategorieService } from 'src/app/services/category.service';
 
@@ -10,16 +11,40 @@ import { CategorieService } from 'src/app/services/category.service';
 })
 export class CategoryListComponent implements OnInit {
 
-  categoryList$: Observable<Category[]> | undefined;
+  options: string[] = ['A-Z', 'Z-A']
+  selected = new FormControl()
 
-  constructor(private _service: CategorieService) { }
-  
+  categoryListSubject$ = new BehaviorSubject<Category[]>([])
+  categoryList$: Category[] | undefined
+
+
+  /**
+   * @param _service 
+   */
+  constructor(private _service: CategorieService) {
+
+    this.selected.valueChanges.subscribe(x => {
+      if (!!this.categoryList$)
+        if (x === 'A-Z')
+          this.categoryListSubject$.next(this.categoryList$.sort(
+            (a: Category, b: Category) => a.name > b.name ? 1 : -1))
+        else {
+          this.categoryListSubject$.next(this.categoryList$.sort(
+            (a: Category, b: Category) => a.name > b.name ? -1 : 1))
+        }
+    })
+  }
+
   /*
     Catagories will load from api when the component is initialized and filter only first Ten categories.
     Then categoryList will be used directly in the template using Async Pipe.
   */
   ngOnInit(): void {
-    this.categoryList$ = this._service.getAllCategories().pipe(map((x: Category[]) => x.slice(0, 10)))
+    this._service.getAllCategories().pipe(map((x: Category[]) => x.slice(0, 10))).subscribe(res => {
+      this.categoryListSubject$.next(res)
+      this.selected.setValue('A-Z')
+    })
+    this.categoryListSubject$.subscribe(x => this.categoryList$ = x)
   }
 
 }
